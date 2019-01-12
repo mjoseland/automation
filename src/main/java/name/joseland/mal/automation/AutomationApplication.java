f13735180c089eeca3cda8b2899b5f115dd59a5d
Mal Joseland <mal@joseland.name>
2019-01-12 12:47:36 +1000

    Empty runnable project

---
+++ src/main/java/name/joseland/mal/automation/AutomationApplication.java
@@ -0,0 +1,14 @@
+package name.joseland.mal.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationApplication.class, args);
+    }
+
+}
+
