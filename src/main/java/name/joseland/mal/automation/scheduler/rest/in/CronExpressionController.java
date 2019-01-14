6dfd6eb26b9ad8fa6a4683c0a781e7a742cc3f5a
Mal Joseland <mal@joseland.name>
2019-01-14 21:52:17 +1000

    added to scheduler.rest.in: CronExpressionController and CronExpressionResourceAssembler

---
+++ src/main/java/name/joseland/mal/automation/scheduler/rest/in/CronExpressionController.java
@@ -0,0 +1,98 @@
+package name.joseland.mal.automation.scheduler.rest.in;
+
+import name.joseland.mal.automation.scheduler.db.CronExpression;
+import name.joseland.mal.automation.scheduler.db.CronExpressionRepository;
+import name.joseland.mal.automation.core.rest.in.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping
+public class CronExpressionController {
+
+    private final CronExpressionRepository repository;
+    private final CronExpressionResourceAssembler resourceAssembler;
+
+    CronExpressionController(@Autowired CronExpressionRepository repository,
+                             @Autowired CronExpressionResourceAssembler resourceAssembler) {
+        this.repository = repository;
+        this.resourceAssembler = resourceAssembler;
+    }
+
+
+    /**************************************************************************************************************/
+    /*********************************************** PUBLIC METHODS ***********************************************/
+    /**************************************************************************************************************/
+
+
+    @GetMapping("/cronexpressions")
+    public Resources<Resource<CronExpression>> retrieveAll() {
+        List<Resource<CronExpression>> cronExpressions = repository.findAll().stream()
+                .map(resourceAssembler::toResource)
+                .collect(Collectors.toList());
+
+        return new Resources<>(cronExpressions,
+                linkTo(methodOn(CronExpressionController.class).retrieveAll()).withSelfRel());
+    }
+
+    @PostMapping("/cronexpressions")
+    public ResponseEntity<?> create(@RequestBody CronExpression cronExpression) throws URISyntaxException {
+        Resource<CronExpression> resource = resourceAssembler.toResource(repository.save(cronExpression));
+
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
+    }
+
+    @GetMapping("/cronexpressions/{id}")
+    public Resource<CronExpression> retrieve(@PathVariable int id) {
+        Optional<CronExpression> cronExpressionOpt = repository.findById(id);
+
+        if (cronExpressionOpt.isEmpty())
+            throw new ResourceNotFoundException("CronExpression", id);
+
+        return resourceAssembler.toResource(cronExpressionOpt.get());
+    }
+
+    @PutMapping("/cronexpressions/{id}")
+    public ResponseEntity<?> update(@RequestBody CronExpression newCronExpression,
+                                    @PathVariable int id) throws URISyntaxException {
+        CronExpression savedCronExpression = repository.findById(id)
+                .map(cronExpression -> {
+                    cronExpression.setExpression(newCronExpression.getExpression());
+                    cronExpression.setDescription(newCronExpression.getDescription());
+
+                    return repository.save(cronExpression);
+                }).orElseGet(() -> {
+                    newCronExpression.setId(id);
+
+                    return repository.save(newCronExpression);
+                });
+
+        Resource<CronExpression> resource = resourceAssembler.toResource(savedCronExpression);
+
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
+    }
+
+    @DeleteMapping("/cronexpressions/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/name/joseland/mal/automation/scheduler/rest/in/CronExpressionResourceAssembler.java
@@ -0,0 +1,23 @@
+package name.joseland.mal.automation.scheduler.rest.in;
+
+import name.joseland.mal.automation.scheduler.db.CronExpression;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@Component
+public class CronExpressionResourceAssembler implements ResourceAssembler<CronExpression, Resource<CronExpression>> {
+
+
+    @Override
+    public Resource<CronExpression> toResource(CronExpression cronExpression) {
+        return new Resource<>(cronExpression,
+                linkTo(methodOn(CronExpressionController.class).retrieve(cronExpression.getId()))
+                        .withSelfRel(),
+                linkTo(methodOn(CronExpressionController.class).retrieveAll()).withRel("cronexpressions"));
+    }
+
+}
