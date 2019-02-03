4c11f4b1a533de90d1fa4832324ac41b0b65533d
Mal Joseland <mal@joseland.name>
2019-02-03 13:56:44 +1000

    data api core: added service registration and discovery

--- src/main/java/name/joseland/mal/automation/core/rest/in/ExceptionAdvice.java
+++ src/main/java/name/joseland/mal/automation/core/rest/in/ExceptionAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ExceptionAdvice {
+class ExceptionAdvice {
 
     @ResponseBody
     @ExceptionHandler(ResourceNotFoundException.class)
