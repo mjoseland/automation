6aea5f55c8214055b0db4cf979eb2e7ef674906b
mjoseland <mjoseland@contal.net.au>
2019-07-05 17:31:56 +1000

    modified JsonBodyHandler internals as temporary fix to hanging when reading from input streams

--- src/main/java/name/joseland/mal/automation/core/rest/out/JsonBodyHandler.java
+++ src/main/java/name/joseland/mal/automation/core/rest/out/JsonBodyHandler.java
@@ -1,16 +1,16 @@
 package name.joseland.mal.automation.core.rest.out;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import name.joseland.mal.automation.core.rest.HttpRequestException;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.http.HttpResponse;
 
 /**
  * {@link HttpResponse.BodyHandler} implementation for a response with a JSON-formatted body.
+ *
+ * TODO implement mapping from BodyHandlers::ofInputStream instead of BodyHandlers::ofString
  */
 public class JsonBodyHandler implements HttpResponse.BodyHandler<JsonNode> {
 
@@ -18,15 +18,15 @@ public class JsonBodyHandler implements HttpResponse.BodyHandler<JsonNode> {
      * Returns a {@link HttpResponse.BodySubscriber<JsonNode>} that can convert response body bytes into a
      * {@link JsonNode}.
 	 *
-     * @throws JsonParseException  if the response format can't be parsed as JSON
-	 */
+     * @param responseInfo the info of the response to apply the body handler to
+     */
     @Override
     public HttpResponse.BodySubscriber<JsonNode> apply(HttpResponse.ResponseInfo responseInfo) {
-        HttpResponse.BodySubscriber<InputStream> stringSubscriber = HttpResponse.BodyHandlers.ofInputStream()
+        HttpResponse.BodySubscriber<String> stringSubscriber = HttpResponse.BodyHandlers.ofString()
                 .apply(responseInfo);
 
         return HttpResponse.BodySubscribers.mapping(stringSubscriber,
-                JsonBodyHandler::jsonFromInputStream);
+                JsonBodyHandler::jsonFromString);
     }
 
 
@@ -35,9 +35,9 @@ public class JsonBodyHandler implements HttpResponse.BodyHandler<JsonNode> {
     /* ********************************************************************************************************** */
 
 
-    private static JsonNode jsonFromInputStream(InputStream is) {
+    private static JsonNode jsonFromString(String s) {
         try {
-            return new ObjectMapper().readTree(is);
+            return new ObjectMapper().readTree(s);
         } catch (IOException e) {
             throw new HttpRequestException(e.getMessage());
         }
