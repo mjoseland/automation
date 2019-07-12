f81d11f446161d6e72fca7bf5a3df556f3d3270b
mjoseland <mjoseland@contal.net.au>
2019-07-12 18:48:10 +1000

    InternalRequestTest: changed constant prefix UPDATE_ to OTHER_

--- src/test/java/name/joseland/mal/automation/requestrepo/db/InternalRequestTest.java
+++ src/test/java/name/joseland/mal/automation/requestrepo/db/InternalRequestTest.java
@@ -22,17 +22,17 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @DataJpaTest
 public class InternalRequestTest {
 
-    private static final HttpMethod TEST_HTTP_METHOD = HttpMethod.GET;
-    private static final HttpMethod UPDATE_TEST_HTTP_METHOD = HttpMethod.PUT;
+    public static final HttpMethod TEST_HTTP_METHOD = HttpMethod.GET;
+    public static final HttpMethod OTHER_TEST_HTTP_METHOD = HttpMethod.PUT;
 
-    private static final String TEST_SERVICE_ID = "scheduler";
-    private static final String UPDATE_TEST_SERVICE_ID = "text-source-monitor";
+    public static final String TEST_SERVICE_ID = "scheduler";
+    public static final String OTHER_TEST_SERVICE_ID = "text-source-monitor";
 
-    private static final String TEST_RESOURCE = "/trigger-configs/10";
-    private static final String UPDATE_TEST_RESOURCE = "/http-sources/11/perform-monitor";
+    public static final String TEST_RESOURCE = "/trigger-configs/10";
+    public static final String OTHER_TEST_RESOURCE = "/http-sources/11/perform-monitor";
 
-    private static final JsonNode TEST_BODY = BooleanNode.TRUE;
-    private static final JsonNode UPDATE_TEST_BODY = BooleanNode.FALSE;
+    public static final JsonNode TEST_BODY = BooleanNode.TRUE;
+    public static final JsonNode OTHER_TEST_BODY = BooleanNode.FALSE;
 
 
     @ClassRule
@@ -50,13 +50,13 @@ public class InternalRequestTest {
         GenericEntityTester<InternalRequest> tester = GenericEntityTester.buildNew(InternalRequest::new,
                 repository);
 
-        tester.addField(TEST_HTTP_METHOD, UPDATE_TEST_HTTP_METHOD, InternalRequest::getHttpMethod,
+        tester.addField(TEST_HTTP_METHOD, OTHER_TEST_HTTP_METHOD, InternalRequest::getHttpMethod,
                 InternalRequest::setHttpMethod);
-        tester.addField(TEST_SERVICE_ID, UPDATE_TEST_SERVICE_ID, InternalRequest::getServiceId,
+        tester.addField(TEST_SERVICE_ID, OTHER_TEST_SERVICE_ID, InternalRequest::getServiceId,
                 InternalRequest::setServiceId);
-        tester.addField(TEST_RESOURCE, UPDATE_TEST_RESOURCE, InternalRequest::getResource,
+        tester.addField(TEST_RESOURCE, OTHER_TEST_RESOURCE, InternalRequest::getResource,
                 InternalRequest::setResource);
-        tester.addField(TEST_BODY, UPDATE_TEST_BODY, InternalRequest::getBody,
+        tester.addField(TEST_BODY, OTHER_TEST_BODY, InternalRequest::getBody,
                 InternalRequest::setBody);
 
         this.tester = tester;
