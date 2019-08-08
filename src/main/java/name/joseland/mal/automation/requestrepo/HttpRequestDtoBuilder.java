74519d6a97f0da39d817639c144a2da102c4c569
mjoseland <mjoseland@contal.net.au>
2019-08-08 19:02:11 +1000

    added HttpRequestDto (core) and HttpRequestDtoBuilder (requestrepo only)

---
+++ src/main/java/name/joseland/mal/automation/requestrepo/HttpRequestDtoBuilder.java
@@ -0,0 +1,59 @@
+package name.joseland.mal.automation.requestrepo;
+
+import name.joseland.mal.automation.core.rest.out.internal.HttpRequestDto;
+import name.joseland.mal.automation.core.rest.out.internal.ServiceInstanceLocator;
+import name.joseland.mal.automation.core.rest.out.internal.exception.InternalServiceNotFoundException;
+import name.joseland.mal.automation.requestrepo.db.ExternalRequest;
+import name.joseland.mal.automation.requestrepo.db.InternalRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * Contains single-step builders for {@link HttpRequestDto} instances.
+ */
+@Component
+public class HttpRequestDtoBuilder {
+
+	private final ServiceInstanceLocator serviceInstanceLocator;
+
+	HttpRequestDtoBuilder(@Autowired ServiceInstanceLocator serviceInstanceLocator) {
+		this.serviceInstanceLocator = serviceInstanceLocator;
+	}
+
+
+	/**
+	 * Static builder from {@link InternalRequest}.
+	 *
+	 * @param internalRequest	an internal request
+	 * @return					the request DTO
+	 */
+	public HttpRequestDto fromInternalRequest(InternalRequest internalRequest)
+			throws InternalServiceNotFoundException {
+		Objects.requireNonNull(internalRequest);
+
+		// find a ServiceInstance from the internalRequest's serviceId
+		ServiceInstance serviceInstance = serviceInstanceLocator.findInstance(internalRequest.getServiceId());
+
+		String url = serviceInstance.getUri() + internalRequest.getResource();
+
+		return new HttpRequestDto(internalRequest.getHttpMethod(), url,
+				new HashMap<>(), internalRequest.getBody().toString());
+	}
+
+	/**
+	 * Static builder from {@link ExternalRequest}.
+	 *
+	 * @param externalRequest	an external request
+	 * @return					the request DTO
+	 */
+	public HttpRequestDto fromExternalRequest(ExternalRequest externalRequest) {
+		Objects.requireNonNull(externalRequest);
+
+		return new HttpRequestDto(externalRequest.getHttpMethod(), externalRequest.getUrl(),
+				externalRequest.getHeaderFields(), externalRequest.getBody());
+	}
+}
